126a02d9c6da8964670486e27cc11735c5af8d76
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-05 12:37:15 +0500

    post method added to add task

--- todo/src/app/api/todo/route.ts
+++ todo/src/app/api/todo/route.ts
@@ -2,15 +2,31 @@ import { db } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const client = await db.connect();
+  const client = await db.connect();
 
-    try {
-        // await client.sql`CREATE TABLE IF NOT EXISTS todos(id serial, tasks varchar(255))` // to create table using code
-        // await client.sql`DROP TABLE todos` // to drop table
-        return NextResponse.json({message: "You called this api"});
-    } catch (error) {
-        console.log('error', error)
-        return new NextResponse("Something went wrong");
+  try {
+    // await client.sql`CREATE TABLE IF NOT EXISTS todos(id serial, task varchar(255))` // to create table using code
+    // await client.sql`DROP TABLE todos` // to drop table
+    return NextResponse.json({ message: "You called this api" });
+  } catch (error) {
+    console.log("error", error);
+    return new NextResponse("Something went wrong");
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const client = await db.connect();
+  const req = await request.json();
+  try {
+    if (req.task) {
+      await client.sql`CREATE TABLE IF NOT EXISTS todos(id serial, task varchar(255))`; // to create table using code
+      await client.sql`INSERT INTO todos(task) VALUES(${req.task})`; // to add new task
+      return NextResponse.json({ message: "Task added successfully" });
+    } else {
+      throw new Error("Task field is required");
     }
-
+  } catch (error) {
+    console.log("error", error);
+    return NextResponse.json({message: (error as {message: string}).message});
+  }
 }
